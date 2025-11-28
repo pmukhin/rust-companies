b7d73a5c0d435c379b106e76179798bbb8c00ec6
Pavel Mukhin <pauri.lentonen@gmail.com>
2025-11-28 13:23:01 +0100

    Add contribution guidelines to CONTRIBUTING.rs

---
+++ CONTRIBUTING.rs
@@ -0,0 +1,8 @@
+# How to contribute
+
+Please ensure your pull request adheres to the following guidelines:
+
+* Companies and categories are sorted lexicographically 
+* Companies must use Rust in their stack and be hiring (or have hired) Rust developers within the past year.
+
+Thank you!
